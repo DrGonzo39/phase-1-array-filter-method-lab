9fa27d08329cd8ecc4e015ad2b48f3ba9b8a050d
Arthur Benware <artpolitic91@gmail.com>
2022-09-28 19:23:35 -0400

    commit tree eb2b34b1ffbf4def51a596277456c249459cd212

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching (drivers,name) {
+    return drivers.filter(string => string.toUpperCase() === name.toUpperCase());
+}; 
+
+function fuzzyMatch (drivers,string) {
+    return drivers.filter(name => name.indexOf(string) === 0);
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
